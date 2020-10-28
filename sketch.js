fb1b2bd0ab09d1b182bf8e3a055612a1d17c9d49
purajit26 <71131375+purajit26@users.noreply.github.com>
2020-10-28 23:32:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+
+function preload()
+{
+  //load images here
+  doglmgimg =loadImage("images/doglmg.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+if(KeyWentDown(UP_ARROW)){
+writesteock(food);
+dog.addImage(dogHappy)
+}
+}
+
+
+
